534290a44b0c63b269cec8bbcdc654d7a531e9ef
Paul Bleisch <pbleisch@acm.org>
2024-07-14 16:47:40 -0700

    Remove STD usage

--- misc/node-exporter-install.sh
+++ misc/node-exporter-install.sh
@@ -34,14 +34,14 @@ msg_error() {
 
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
+apt-get install -y curl
+apt-get install -y sudo
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Node Exporter"
 RELEASE=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-$STD wget https://github.com/prometheus/node_exporter/releases/download/v${RELEASE}/node_exporter-${RELEASE}.linux-amd64.tar.gz
-$STD tar -xvf node_exporter-${RELEASE}.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v${RELEASE}/node_exporter-${RELEASE}.linux-amd64.tar.gz
+tar -xvf node_exporter-${RELEASE}.linux-amd64.tar.gz
 cd node_exporter-${RELEASE}.linux-amd64
 mv node_exporter /usr/local/bin/
 msg_ok "Installed Node Exporter"
@@ -61,11 +61,11 @@ ExecStart=/usr/local/bin/node_exporter
 
 [Install]
 WantedBy=multi-user.target" >$service_path
-$STD sudo systemctl enable --now node_exporter
+sudo systemctl enable --now node_exporter
 msg_ok "Created Service"
 
 msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+apt-get -y autoremove
+apt-get -y autoclean
 rm -rf ../node_exporter-${RELEASE}.linux-amd64 ../node_exporter-${RELEASE}.linux-amd64.tar.gz
 msg_ok "Cleaned"
